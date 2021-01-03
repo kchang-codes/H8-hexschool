c8282336266038f31104dc11468690aa0890a051
DESKTOP-LDGUA49\kate <kchang.codes@gmail.com>
2021-01-03 22:43:31 +0800

    Update 2021-01-03T14:43:24.519Z

--- app/assets/js/all.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-  $('.navbar-toggler').click(function () {
-      $(this).toggleClass('open');
-  });
-});
---
+++ assets/js/all.js
@@ -0,0 +1,8 @@
+"use strict";
+
+$(document).ready(function () {
+  $('.navbar-toggler').click(function () {
+    $(this).toggleClass('open');
+  });
+});
+//# sourceMappingURL=all.js.map
